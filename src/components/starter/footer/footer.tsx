9a25c99b905107cb637470bdc85fdf4c48c6942f
carlosqdev <carlosqdev@gmail.com>
2023-06-06 20:21:56 -0600

    modified footer component

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,6 +1,6 @@
-import { component$ } from '@builder.io/qwik';
-import { useServerTimeLoader } from '~/routes/layout';
-import styles from './footer.module.css';
+import { component$ } from "@builder.io/qwik";
+import { useServerTimeLoader } from "~/routes/layout";
+import styles from "./footer.module.css";
 
 export default component$(() => {
   const serverTime = useServerTimeLoader();
@@ -9,7 +9,7 @@ export default component$(() => {
     <footer>
       <div class="container">
         <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
+          <span>Power by Qwik</span>
           <span class={styles.spacer}>|</span>
           <span>{serverTime.value.date}</span>
         </a>
