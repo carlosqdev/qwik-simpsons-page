6c93eedce6eaaf9ee17889e464520ad7041a95fa
carlosqdev <carlosqdev@gmail.com>
2023-06-06 21:22:23 -0600

    added types

---
+++ src/types.d.ts
@@ -0,0 +1,23 @@
+export interface Characters {
+  docs: Character[];
+  totalDocs: number;
+  limit: number;
+  totalPages: number;
+  page: number;
+  pagingCounter: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: null;
+  nextPage: number;
+}
+
+export interface Character {
+  _id: string;
+  Nombre: string;
+  Historia: string;
+  Imagen: string;
+  Genero: string;
+  Estado: string;
+  Ocupacion: string;
+  updatedAt?: Date;
+}
