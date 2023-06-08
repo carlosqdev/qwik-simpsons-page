90023eabb318f9bbdafb235d9a19766cf12f3452
carlosqdev <carlosqdev@gmail.com>
2023-06-07 21:09:40 -0600

    styles footer

--- src/components/starter/footer/footer.tsx
+++ src/components/starter/footer/footer.tsx
@@ -1,17 +1,27 @@
 import { component$ } from "@builder.io/qwik";
 import { useServerTimeLoader } from "~/routes/layout";
-import styles from "./footer.module.css";
 
 export default component$(() => {
   const serverTime = useServerTimeLoader();
 
   return (
-    <footer>
-      <div class="container">
-        <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Power by Qwik</span>
-          <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
+    <footer
+      class="bg-yellow-400 py-8 border-t border-t-yellow-500
+    "
+    >
+      <div class="w-[90%] lg:w-[70%] m-auto">
+        <a
+          class="flex justify-center gap-2"
+          href="https://www.builder.io/"
+          target="_blank"
+        >
+          <span class="text-xl font-sans font-bold text-slate-950">
+            Power by Qwik
+          </span>
+          <span class="text-xl font-sans font-bold text-slate-950">|</span>
+          <span class="text-xl font-sans font-bold text-slate-950">
+            {serverTime.value.date}
+          </span>
         </a>
       </div>
     </footer>
